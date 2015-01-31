17b03d28128cedd92a6078d35a4066e2ecfdf155
Jesse Stuart <jesse@jessestuart.ca>
2015-01-31 11:58:43 -0800

    pkg/sse: wip
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- pkg/sse/stream.go
+++ pkg/sse/stream.go
@@ -9,32 +9,49 @@ import (
 	hh "github.com/flynn/flynn/pkg/httphelper"
 )
 
+type identifiable interface {
+	GetID() string
+}
+
 type Stream struct {
 	w         *Writer
+	rw        http.ResponseWriter
+	ch        interface{}
 	closeChan chan struct{}
 	doneChan  chan struct{}
 	closed    bool
+	Done      chan struct{}
+}
+
+func NewStream(w http.ResponseWriter, ch interface{}) *Stream {
+	sw := NewWriter(w)
+	return &Stream{rw: w, w: sw, ch: ch, closeChan: make(chan struct{}), doneChan: make(chan struct{}), Done: make(chan struct{})}
 }
 
 func ServeStream(w http.ResponseWriter, ch interface{}) *Stream {
-	sw := NewWriter(w)
-	w.Header().Set("Content-Type", "text/event-stream; charset=utf-8")
-	w.WriteHeader(200)
-	sw.Flush()
+	s := NewStream(w, ch)
+	s.Serve()
+	s.Wait()
+	return s
+}
 
-	s := &Stream{w: sw, closeChan: make(chan struct{}), doneChan: make(chan struct{})}
-	wr := hh.FlushWriter{Writer: NewWriter(w), Enabled: true}
+func (s *Stream) Serve() {
+	s.rw.Header().Set("Content-Type", "text/event-stream; charset=utf-8")
+	s.rw.WriteHeader(200)
+	s.w.Flush()
+
+	wr := hh.FlushWriter{Writer: NewWriter(s.rw), Enabled: true}
 	enc := json.NewEncoder(wr)
 
 	go func() {
-		if cw, ok := w.(http.CloseNotifier); ok {
+		if cw, ok := s.rw.(http.CloseNotifier); ok {
 			<-cw.CloseNotify()
 			s.Close()
 		}
 	}()
 
 	closeChanValue := reflect.ValueOf(s.closeChan)
-	chValue := reflect.ValueOf(ch)
+	chValue := reflect.ValueOf(s.ch)
 	go func() {
 		defer s.done()
 		for {
@@ -59,7 +76,8 @@ func ServeStream(w http.ResponseWriter, ch interface{}) *Stream {
 				s.sendKeepAlive()
 			default:
 				if ok {
-					if err := enc.Encode(v.Interface()); err != nil {
+					if err := s.send(enc, v.Interface()); err != nil {
+						// TODO: report error
 						return
 					}
 				} else {
@@ -68,19 +86,23 @@ func ServeStream(w http.ResponseWriter, ch interface{}) *Stream {
 			}
 		}
 	}()
-
-	s.wait()
-
-	return s
 }
 
-func (s *Stream) wait() {
+func (s *Stream) Wait() {
 	<-s.doneChan
 }
 
 func (s *Stream) done() {
 	close(s.doneChan)
+	close(s.Done)
 	s.Close()
+}
+
+func (s *Stream) send(enc *json.Encoder, v interface{}) error {
+	if i, ok := v.(identifiable); ok {
+		s.w.writeID(i.GetID())
+	}
+	return enc.Encode(v)
 }
 
 func (s *Stream) sendKeepAlive() error {
@@ -96,7 +118,7 @@ func (s *Stream) Close() {
 	if !s.closed {
 		s.closed = true
 		close(s.closeChan)
-		s.wait()
+		s.Wait()
 	}
 }
 
